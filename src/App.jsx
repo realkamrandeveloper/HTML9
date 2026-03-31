b85ae3d139b297a41e12d81f27b34728d63b3b31
realkamrandeveloper <kamrantahirislam@gmail.com>
2026-03-31 13:54:04 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+import Table from "./components/Table";
+
+export default function App(){
+    return(
+        <>
+        <Header/>
+        <Table/>
+        </>
+    );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+    return(
+        <>
+        <h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Periodic Table of the Elements</h1>
+        </>
+    );
+}
